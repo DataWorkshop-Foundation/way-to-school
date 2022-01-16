7b0bab2d80c3a99d546858636271fc08c3ce7c1a
Łukasz Sawaniewski <lsawaniewski@ermlab.com>
2022-01-16 21:06:00 +0100

    Geo data handling

--- geodata/adm_units.py
+++ geodata/adm_units.py
@@ -1,4 +1,5 @@
 import os
+
 import geopandas as geo
 from geopandas import GeoDataFrame
 
@@ -16,9 +17,24 @@ class AdmUnits:
         assert os.path.exists(data_path), f"Provided {data_path=} does not exist."
 
         self.data_path = data_path
-        self.geo_data = geo.read_file(self.data_path)
-        self.__data_levels = set(self.geo_data["typ"])
+        self.__geo_data = geo.read_file(self.data_path)
+        self.__data_levels = set(self.__geo_data["typ"])
 
-    def __get_data(self, data_level: str) -> GeoDataFrame:
+    def __filter_data(self, data_level: str) -> GeoDataFrame:
         assert data_level in self.__data_levels, f"Provided {data_level=} could not be found in data."
-        return self.geo_data[self.geo_data["typ"] == data_level]
+        return self.__geo_data[self.__geo_data["typ"] == data_level]
+
+    def get_data(self, data_level: str) -> GeoDataFrame:
+        """Returns complete geospatial data for the selected level
+
+        Parameters
+        ----------
+        data_level: str
+            Territorial division of data
+
+        Returns
+        -------
+        geo_data: GeoDataFrame
+        """
+
+        return self.__filter_data(data_level=data_level)
