af20e87613703b2cf9a4b093d01a75143b1b517d
Magdalena Lipka <madziak20@gmail.com>
2022-01-26 19:37:51 +0100

    Prepare defs to build dataset and marge area, populations to basic dataset

--- prep_data_school.py
+++ prep_data_school.py
@@ -1,44 +1,94 @@
+from pathlib import PurePath, PurePosixPath
+
 import pandas as pd
 import unidecode
 
 
-def unidecode_columns(df):
-    col_name = {}
-
-    for i in df.columns:
-        col_name[i] = unidecode.unidecode(i).replace(' ','_').lower()
-
-    df.rename(columns=col_name, inplace=True)
-
-
 def clean_text(x):
-    return str(x).replace('=','').replace('"','')
+    return str(x).replace("=", "").replace('"', "")
 
-def clean_cols(df):
-    for col in df.select_dtypes(['object']).columns:
-        df[col] = df[col].map(lambda x: clean_text(x))
 
-df = pd.read_csv('school.csv')
+def prep_data(path_input):
+    try:
+        df = pd.read_csv(path_input)
 
-unidecode_columns(df)
-clean_cols(df)
+        col_name = {}
 
-for col in df.columns:
-    if df[col].nunique() < 100:
-        #print(f'{col}, unique :' ,df[col].nunique())
-        df[col] = df[col].astype('category')
+        for i in df.columns:
+            col_name[i] = unidecode.unidecode(i).replace(" ", "_").lower()
 
-df['gmina_typ_cat'] = df.gmina.map(lambda x: x.split('(')[1].replace(')',''))
+        df.rename(columns=col_name, inplace=True)
 
-df['gmina_nazwa_cat'] = df.gmina.map(lambda x: x.split('(')[0].replace(')',''))
+        for col in df.select_dtypes(["object"]).columns:
+            df[col] = df[col].map(lambda x: clean_text(x))
 
-df['gmina_typ_cat'] = df['gmina_typ_cat'].astype('category')
+        df["gmina_typ"] = df.gmina.map(lambda x: x.split("(")[1].replace(")", ""))
 
-df['gmina_nazwa_cat'] = df['gmina_nazwa_cat'].astype('category')
+        df["gmina_cat"] = df.gmina.map(lambda x: x.split("(")[0].replace(")", ""))
 
-# convert the 'Date' column to datetime format
-df['data_likwidacji']= pd.to_datetime(df['data_likwidacji'])
+        df.to_csv(path_input.replace(".csv", "_prep.csv"), index=False, encoding="utf-8")
 
-df['data_zalozenia']= pd.to_datetime(df['data_zalozenia'], errors = 'coerce')
+    except FileNotFoundError:
+        print("Nie znaleziono pliku.")
 
-df['data_rozpoczecia_dzialalnosci']= pd.to_datetime(df['data_rozpoczecia_dzialalnosci'], errors = 'coerce')
+
+def marge_area_population(path_input):
+    try:
+        p = PurePath(path_input)
+        path = p.parents[0]
+
+        name = PurePosixPath(path_input).stem
+        name = name + "_prep.csv"
+        full_path = path.joinpath(name)
+
+        if PurePath(path) == PurePath():
+
+            df_school = pd.read_csv(name)
+            df_province = pd.read_csv("populations_area_province.csv")
+            df_community = pd.read_csv("populations_area_community.csv")
+            df_district = pd.read_csv("populations_area_district.csv")
+
+            df_full = pd.merge(
+                df_school.merge(
+                    df_province, how="left", left_on="kod_terytorialny_wojewodztwo", right_on="identyfikator"
+                ).merge(
+                    df_district,
+                    how="left",
+                    left_on="kod_terytorialny_powiat",
+                    right_on="identyfikator",
+                    suffixes=("_prov", "_dist"),
+                ),
+                df_community,
+                how="left",
+                left_on="kod_terytorialny_gmina",
+                right_on="identyfikator",
+                suffixes=(None, "_comm"),
+            )
+            df_full.to_csv(name, index=False, encoding="utf-8")
+
+        else:
+            df_school = pd.read_csv(full_path)
+            df_province = pd.read_csv(path.joinpath("populations_area_province.csv"))
+            df_community = pd.read_csv(path.joinpath("populations_area_community.csv"))
+            df_district = pd.read_csv(path.joinpath("populations_area_district.csv"))
+
+            df_full = pd.merge(
+                df_school.merge(
+                    df_province, how="left", left_on="kod_terytorialny_wojewodztwo", right_on="identyfikator"
+                ).merge(
+                    df_district,
+                    how="left",
+                    left_on="kod_terytorialny_powiat",
+                    right_on="identyfikator",
+                    suffixes=("_prov", "_dist"),
+                ),
+                df_community,
+                how="left",
+                left_on="kod_terytorialny_gmina",
+                right_on="identyfikator",
+                suffixes=(None, "_comm"),
+            )
+            df_full.to_csv(full_path, index=False, encoding="utf-8")
+
+    except FileNotFoundError:
+        print("Nie znaleziono pliku.")
