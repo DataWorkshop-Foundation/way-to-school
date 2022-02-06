f5fbb5405999207fbbcad01d67c6aa55409d143a
MalarzDawid <github@dawidmalarz.pl>
2022-02-06 12:13:01 +0100

    Add docs

--- tools/scraper.py
+++ tools/scraper.py
@@ -9,6 +9,15 @@ import requests
 
 class CoordScraper:
     def __init__(self, filepath: str, timeout: Optional[int] = 1) -> None:
+        """Class for download school coords from OSM.
+
+        Parameters
+        ----------
+        filepath : str
+            Path to csv file
+        timeout : Optional[int], optional
+            Timeout between requests, by default 1 second
+        """
         assert os.path.exists(filepath), f"Provided {filepath} does not exist."
 
         self.filepath = filepath
@@ -34,6 +43,18 @@ class CoordScraper:
         return {value: str(row[value]) for value in list(values)}
 
     def get_coords(self, query_dict: dict) -> List[float]:
+        """Create request based on specific details
+
+        Parameters
+        ----------
+        query_dict : dict
+            Query details
+
+        Returns
+        -------
+        List[float]
+            Returns school coordinates
+        """
         query = " ".join([*query_dict.values()])
         logging.info("Query: ", query)
         response = requests.get(f"https://photon.komoot.io/api/?q={query}").json()
@@ -52,7 +73,7 @@ class CoordScraper:
 
         return [coord for coord in response["features"][0]["geometry"]["coordinates"]]
 
-    def get_from_osm(self, checkpoint: Optional[int] = 50):
+    def get_from_osm(self, checkpoint: Optional[int] = 50) -> None:
         try:
             for idx, row in self.df.iterrows():
                 query_dict = self.query(
