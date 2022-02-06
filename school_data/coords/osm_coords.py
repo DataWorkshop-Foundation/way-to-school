1fdaf63ac11e57e6f9210dbf34548e0e8948ef13
Łukasz Sawaniewski <lsawaniewski@ermlab.com>
2022-02-06 20:18:49 +0100

    WIP: CoordScraper

--- school_data/coords/osm_coords.py
+++ school_data/coords/osm_coords.py
@@ -1,27 +1,31 @@
 import logging
 import os
 import time
-from typing import List, Optional
+from typing import Dict, Iterable, List, Optional
 
 import pandas as pd
 import requests
+from tqdm import tqdm
 
 
 class CoordScraper:
-    def __init__(self, filepath: str, timeout: Optional[int] = 1) -> None:
+    FIELD_NAMES = [
+        "wojewodztwo",
+        "miejscowosc",
+        "ulica",
+        "numer_budynku",
+        "kod_pocztowy",
+    ]
+
+    def __init__(self, timeout: Optional[int] = 1) -> None:
         """Class for download school coords from OSM.
 
         Parameters
         ----------
-        filepath : str
-            Path to csv file
         timeout : Optional[int], optional
             Timeout between requests, by default 1 second
         """
-        assert os.path.exists(filepath), f"Provided {filepath} does not exist."
 
-        self.filepath = filepath
-        self.df = pd.read_csv(self.filepath)
         self.timeout = timeout
         self.osm_values = [
             "college",
@@ -34,20 +38,22 @@ class CoordScraper:
             "university",
         ]
 
-    def create_checkpoint(self) -> None:
-        self.df.to_csv(self.filepath)
-        logging.info("Save data...")
-        self.df = pd.read_csv(self.filepath)
+    @staticmethod
+    def __save_csv(data: pd.DataFrame, filepath: str):
+        logging.info(f"Saving data to {filepath=}")
+        data.to_csv(filepath, index=False)
+        logging.info(f"Remaining {sum(data[['lat', 'lng']].isna().any(axis=1))} empty coords.")
 
-    def query(self, row: pd.Series, *values) -> dict:
-        return {value: str(row[value]) for value in list(values)}
+    @staticmethod
+    def __get_query_dict(row: pd.Series, field_names: Iterable[str]) -> Dict[str, str]:
+        return {field_name: str(row.get(field_name, "")) for field_name in field_names}
 
-    def get_coords(self, query_dict: dict) -> List[float]:
+    def get_coords(self, query_dict: Dict[str, str]) -> List[float]:
         """Download coords based on specific query details
 
         Parameters
         ----------
-        query_dict : dict
+        query_dict : Dict[str, str]
             Query details
 
         Returns
@@ -55,46 +61,69 @@ class CoordScraper:
         List[float]
             Returns school coordinates
         """
-        query = " ".join([*query_dict.values()])
-        logging.info("Query: ", query)
+        query = " ".join(query_dict.values())
+        logging.debug(f"Query: {query}")
         response = requests.get(f"https://photon.komoot.io/api/?q={query}").json()
 
-        if len(response["features"]) == 0:
+        if not response.get("features"):
             return None, None
 
-        for idx, feature in enumerate(response["features"]):
+        for feature in response["features"]:
             if all(
                 [
                     feature["properties"]["osm_value"] in self.osm_values,
                     feature["properties"]["postcode"] == query_dict["kod_pocztowy"],
                 ]
             ):
-                return [coord for coord in response["features"][idx]["geometry"]["coordinates"]]
+                return [coord for coord in feature["geometry"]["coordinates"]]
 
         return [coord for coord in response["features"][0]["geometry"]["coordinates"]]
 
-    def get_from_osm(self, checkpoint: Optional[int] = 50) -> None:
-        try:
-            for idx, row in self.df.iterrows():
-                query_dict = self.query(
-                    row,
-                    "wojewodztwo",
-                    "miejscowosc",
-                    "ulica",
-                    "numer_budynku",
-                    "kod_pocztowy",
-                )
-                lat, lng = self.get_coords(query_dict)
-                time.sleep(self.timeout)
-                self.df.loc[idx, "lat"] = lat
-                self.df.loc[idx, "lng"] = lng
+    @staticmethod
+    def __row_has_coords(row: pd.Series) -> bool:
+        return not row[["lat", "lng"]].isna().any()
 
-                if idx % checkpoint == 0:
-                    self.create_checkpoint()
-        except Exception as e:
-            logging.warning(e)
+    def process_csv(self, filepath: str, checkpoint: int = 50):
+        """Collect coordinates for .csv dataset
+
+        Parameters
+        ----------
+        filepath: str
+            File to process
+        checkpoint: int
+            Number of rows after which file will be overwritten
+        """
+
+        assert os.path.exists(filepath), f"Provided {filepath} does not exist."
+        data = pd.read_csv(filepath)
+
+        for col in ("lat", "lng"):
+            if col not in data:
+                data[col] = None
+
+        for row_idx in tqdm(range(len(data))):
+            row = data.iloc[row_idx]
+
+            if self.__row_has_coords(row):
+                continue
+
+            time.sleep(self.timeout)
+            query_dict = self.__get_query_dict(
+                row=row,
+                field_names=self.FIELD_NAMES,
+            )
+            lat, lng = self.get_coords(query_dict)
+
+            if lat and lng:
+                data.iloc[row_idx, data.columns.get_indexer(["lat", "lng"])] = lat, lng
+
+            if row_idx % checkpoint == 0 or row_idx == len(data) - 1:
+                self.__save_csv(data, filepath)
 
 
 if __name__ == "__main__":
-    scraper = CoordScraper("data/school_prep.csv")
-    scraper.get_from_osm()
+    scraper = CoordScraper(timeout=1)
+    scraper.process_csv(
+        filepath="/home/lsawaniewski/Documents/private/docs/DataWorkshop/DW_Olsztyn/repos/way-to-school/data/school_prep.csv",
+        checkpoint=50,
+    )
