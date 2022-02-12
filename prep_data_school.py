d295f587994a3d7e843f0c62a788d415fd6d61fb
Magdalena Lipka <madziak20@gmail.com>
2022-02-12 15:28:57 +0100

    make changes with review

--- prep_data_school.py
+++ prep_data_school.py
@@ -30,9 +30,9 @@ def prep_data(path_input: str, path_output: Optional[str] = None):
     Parameters
     ----------
     path_input
-        Input csv path
+        Path directory/folder
     path_output
-        Optional output csv path
+        Optional output csv Path directory/folder
     """
     file_input = "school.csv"
     file_output = "school_prep.csv"
@@ -40,10 +40,12 @@ def prep_data(path_input: str, path_output: Optional[str] = None):
 
     if not os.path.exists(path_input):
         # raise FileExistsError
-        print("Sciezka nie istnieje!")
+        print("Path not exists!")
+        return
 
-    if os.path.join(path_input, file_input) is None:
-        print("Plik school.csv nie isnieje!")
+    if not os.path.join(path_input, file_input):
+        print("File school.csv not exists!")
+        return
 
     if path_output is None:
         path_output = os.path.join(path_input, file_output)
@@ -52,25 +54,20 @@ def prep_data(path_input: str, path_output: Optional[str] = None):
 
     path_input = os.path.join(path_input, file_input)
 
-    try:
-        df = pd.read_csv(path_input)
+    df = pd.read_csv(path_input)
 
-        col_name = {x: unidecode.unidecode(x).replace(" ", "_").lower() for x in df.columns}
+    col_name = {x: unidecode.unidecode(x).replace(" ", "_").lower() for x in df.columns}
 
-        df.rename(columns=col_name, inplace=True)
+    df.rename(columns=col_name, inplace=True)
 
-        df[["gmina_typ", "gmina_cat"]] = df["gmina"].str.split(" ", 1, expand=True)
+    df[["gmina_typ", "gmina_cat"]] = df["gmina"].str.split(" ", 1, expand=True)
 
-        for col in df.select_dtypes(["object"]).columns:
-            df[col] = df[col].map(lambda x: remove_characters(str(x), ['"', "=", "(", ")"]))
+    for col in df.select_dtypes(["object"]).columns:
+        df[col] = df[col].map(lambda x: remove_characters(str(x), ['"', "=", "(", ")"]))
 
-        df.to_csv(path_output, index=False, encoding="utf-8")
+    df.to_csv(path_output, index=False, encoding="utf-8")
 
-    except FileNotFoundError:
-        print("Not found file.")
-
-    else:
-        print(f"Dataset created, {path_output=}")
+    print(f"Dataset created, {path_output=}")
 
 
 def marge_area_population(path_input: str, path_output: Optional[str] = None):
@@ -79,9 +76,9 @@ def marge_area_population(path_input: str, path_output: Optional[str] = None):
     Parameters
     ----------
     path_input
-        Input csv path
+        Input directory/folder
     path_output
-        Optional output csv path
+        Optional output directory/folder
     """
 
     data_files = [
@@ -94,45 +91,42 @@ def marge_area_population(path_input: str, path_output: Optional[str] = None):
     # assert all([os.path.exists(os.path.join(data_file)) for data_file in data_files])
     if not os.path.exists(path_input):
         # raise FileExistsError
-        print("Sciezka nie istnieje!")
+        print("Path not exists!")
+        return
 
     if not all([os.path.join(path_input, data_file) for data_file in data_files]):
-        print("No files to marge")
+        print("No files to marge!")
+        return
 
-    if path_output is None:
+    if not path_output:
         path_output = os.path.join(path_input)
 
-    try:
-        df_school = pd.read_csv(os.path.join(path_input, "school_prep.csv"))
-        df_province = pd.read_csv(os.path.join(path_input, "populations_area_province.csv"))
-        df_community = pd.read_csv(os.path.join(path_input, "populations_area_community.csv"))
-        df_district = pd.read_csv(os.path.join(path_input, "populations_area_district.csv"))
+    df_school = pd.read_csv(os.path.join(path_input, "school_prep.csv"))
+    df_province = pd.read_csv(os.path.join(path_input, "populations_area_province.csv"))
+    df_community = pd.read_csv(os.path.join(path_input, "populations_area_community.csv"))
+    df_district = pd.read_csv(os.path.join(path_input, "populations_area_district.csv"))
 
-        df_full = pd.merge(
-            df_school.merge(
-                df_province, how="left", left_on="kod_terytorialny_wojewodztwo", right_on="identyfikator"
-            ).merge(
-                df_district,
-                how="left",
-                left_on="kod_terytorialny_powiat",
-                right_on="identyfikator",
-                suffixes=("_prov", "_dist"),
-            ),
-            df_community,
+    df_full = pd.merge(
+        df_school.merge(
+            df_province, how="left", left_on="kod_terytorialny_wojewodztwo", right_on="identyfikator"
+        ).merge(
+            df_district,
             how="left",
-            left_on="kod_terytorialny_gmina",
+            left_on="kod_terytorialny_powiat",
             right_on="identyfikator",
-            suffixes=(None, "_comm"),
-        )
+            suffixes=("_prov", "_dist"),
+        ),
+        df_community,
+        how="left",
+        left_on="kod_terytorialny_gmina",
+        right_on="identyfikator",
+        suffixes=(None, "_comm"),
+    )
 
-        df_full.to_csv(os.path.join(path_output, "school_prep.csv"), index=False, encoding="utf-8")
+    df_full.to_csv(os.path.join(path_output, "school_prep.csv"), index=False, encoding="utf-8")
 
-    except FileNotFoundError:
-        print("Not found files.")
-
-    else:
-        output_path = os.path.join(path_output, "school_prep.csv")
-        print(f"Magres successfull, {output_path=}")
+    output_path = os.path.join(path_output, "school_prep.csv")
+    print(f"Magres successfull, {output_path=}")
 
 
 if __name__ == "__main__":
@@ -145,8 +139,5 @@ if __name__ == "__main__":
     input_path = args.Path
     input_path = abspath(dirname(input_path))
 
-    if not os.path.exists(input_path):
-        print("Sciezka nie istnieje!")
-    else:
-        prep_data(input_path)
-        marge_area_population(input_path)
+    prep_data(input_path)
+    marge_area_population(input_path)
