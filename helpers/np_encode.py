ad3efc33387de60d7f74117b87d562a6ac844e1e
Łukasz Sawaniewski <lsawaniewski@ermlab.com>
2022-02-20 18:50:30 +0100

    Add helper function to cast numpy types

---
+++ helpers/np_encode.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def cast_to_serializable(obj):
+    if isinstance(obj, np.integer):
+        return int(obj)
+    if isinstance(obj, np.floating):
+        return float(obj)
+    if isinstance(obj, np.ndarray):
+        return obj.tolist()
+    return obj
