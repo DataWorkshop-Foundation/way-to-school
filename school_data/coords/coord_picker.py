bdb667f4637538c4be2937bb9a4aa6c6125343b1
Łukasz Sawaniewski <lsawaniewski@ermlab.com>
2022-03-01 21:04:28 +0100

    CoordsPicker with Magda's speedup ;)

--- school_data/coords/coord_picker.py
+++ school_data/coords/coord_picker.py
@@ -5,49 +5,74 @@ import pandas as pd
 from tqdm import tqdm
 
 
-class DummyPicker:
+class BasePicker:
     def __init__(self, csv_filepath: str, coords_filepath: str, id_col):
         self.csv_filepath = csv_filepath
         self.coords_filepath = coords_filepath
         self.id_col = id_col
+        self.__create_coords_dataset()
 
+    def __create_coords_dataset(self):
         with jsonlines.open(self.coords_filepath, "r") as reader:
-            self.dataset = {row[self.id_col]: row["results"] for row in reader}
+            # self.dataset = {row[self.id_col]: row["results"] for row in reader}
+            # self.dataset = coords_data = {k: tuple(v["features"][0]["geometry"]["coordinates"]) for k,v in self.dataset.items() if v["features"]}
+            self.dataset = {
+                row[self.id_col]: tuple(row["results"]["features"][0]["geometry"]["coordinates"])
+                for row in tqdm(reader, desc="Reading jsonl data...")
+                if row["results"]["features"]
+            }
 
-    def get_coords(self, rspo_code: int) -> Tuple[float, float]:
-        if rspo_code in self.dataset.keys():
-            row = self.dataset[rspo_code]
-            if len(row["features"]) > 0:
-                return row["features"][0]["geometry"]["coordinates"]
+    # def get_coords(self, rspo_code: int) -> Tuple[float, float]:
+    #     # if rspo_code in self.dataset.keys():
+    #     row = self.dataset.get(rspo_code)
+    #     if row is not None:
+    #         if len(row["features"]) > 0:
+    #             return row["features"][0]["geometry"]["coordinates"]
 
     def process(self, checkpoint) -> None:
         df = pd.read_csv(self.csv_filepath)
-        for col in ("lat", "lng"):
-            if col not in df:
-                df[col] = None
+        # for col in ("lat", "lng"):
+        #     if col not in df:
+        #         df[col] = None
 
-        for row_idx in tqdm(range(len(df))):
-            df_row = df.iloc[row_idx]
-            values = self.get_coords(df_row[self.id_col])
-            if values is None:
-                continue
+        df[["lat", "lon"]] = pd.DataFrame(df[self.id_col].map(self.dataset).to_list(), index=df.index)
 
-            lat, lng = values
-            df.iloc[row_idx, df.columns.get_indexer(["lat", "lng"])] = lat, lng
+        # for row_idx, coord_idx in tqdm(enumerate(df[self.id_col].values)):
+            # df_row = df.iloc[row_idx]
+            # values = self.get_coords(df_row[self.id_col])
 
-            if row_idx % checkpoint == 0:
-                df.to_csv(self.csv_filepath, index=False)
+            # values = self.get_coords(coord_idx)
+            # if values is None:
+            #     continue
+
+            # lat, lng = values
+            # df.iloc[row_idx, df.columns.get_indexer(["lat", "lng"])] = lat, lng
+
+            # if row_idx % checkpoint == 0:
+            #     df.to_csv(self.csv_filepath, index=False)
+
+        df.to_csv(self.csv_filepath, index=False)
+
+
+class SmarterPicker(BasePicker):
+    def __create_coords_dataset(self):
+        raise NotImplementedError
 
 
 if __name__ == "__main__":
-    import argparse
+    # import argparse
 
-    arg_parser = argparse.ArgumentParser(description="Pick school coords from geocode data")
-    arg_parser.add_argument("--csv_in_path", type=str, help="path to csv file to process", required=True)
-    arg_parser.add_argument("--jsonln_in_path", type=str, help="path to jsonline file to process", required=True)
-    arg_parser.add_argument("--id_col", type=str, defaul="numer_rspo", help="id column name", required=False)
-    arg_parser.add_argument("--checkpoints_step", type=int, default=100, required=False)
-    args = arg_parser.parse_args()
-
-    picker = DummyPicker(csv_filepath=args.csv_in_path, coords_filepath=args.jsonln_in_path, id_col=args.id_col)
-    picker.process(checkpoint=args.checkpoints)
+    # arg_parser = argparse.ArgumentParser(description="Pick school coords from geocode data")
+    # arg_parser.add_argument("--csv_in_path", type=str, help="path to csv file to process", required=True)
+    # arg_parser.add_argument("--jsonln_in_path", type=str, help="path to jsonline file to process", required=True)
+    # arg_parser.add_argument("--id_col", type=str, defaul="numer_rspo", help="id column name", required=False)
+    # arg_parser.add_argument("--checkpoints_step", type=int, default=100, required=False)
+    # args = arg_parser.parse_args()
+    #
+    # picker = DummyPicker(csv_filepath=args.csv_in_path, coords_filepath=args.jsonln_in_path, id_col=args.id_col)
+    picker = BasePicker(
+        csv_filepath="/home/lsawaniewski/Documents/private/docs/DataWorkshop/DW_Olsztyn/repos/way-to-school/data/school_prep.csv",
+        coords_filepath="/home/lsawaniewski/Documents/private/docs/DataWorkshop/DW_Olsztyn/repos/way-to-school/data/school_data.jsonl",
+        id_col="numer_rspo"
+    )
+    picker.process(checkpoint=100)
